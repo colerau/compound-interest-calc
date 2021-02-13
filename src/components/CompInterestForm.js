c38c73fc264ba98db457aa6b796b2b90132ecd42
Cole Rau <colevrau@gmail.com>
2021-02-12 22:05:48 -0800

    app styled. entering testing phase

--- src/components/CompInterestForm.js
+++ src/components/CompInterestForm.js
@@ -25,41 +25,45 @@ const CompInterestForm = props => {
 
   return (
     <div>
-      <h2>Compound Interest Calculator</h2>
+      <h1 className="cool-font">Compound Interest Calculator</h1>
       <form onSubmit={handleSubmit}>
-        <label>
-          Starting amount: $
-          <input type="text" size="18" value={startingAmount} onChange={(e) => setStartingAmount(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Interest rate: &nbsp;
-          <input type="text" size="4" value={interestRate} onChange={(e) => setInterestRate(e.target.value)} />
-          %
-        </label>
-        <br />
-        <label>
-          Number of years: &nbsp;
-          <input type="text" size="3" value={numYears} onChange={(e) => setNumYears(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Compounding interval: &nbsp;
-          <select value={interval} onChange={(e) => setInterval(e.target.value)}>
-            <option value="yearly">yearly</option>
-            <option value="monthly">monthly</option>
-          </select>
-        </label>
-        <br />
-        <br />
-        <input type="submit" value="How much will I have?" />
+          <label className="cool-font">
+            Starting amount:&nbsp;$
+            <input className="cool-font" type="text" size="18" value={startingAmount} onChange={(e) => setStartingAmount(e.target.value)} />
+          </label>
+          <br />
+          <br />
+          <label className="cool-font">
+            Interest rate:&nbsp;
+            <input className="cool-font" type="text" size="4" value={interestRate} onChange={(e) => setInterestRate(e.target.value)} />
+            %
+          </label>
+          <br />
+          <br />
+          <label className="cool-font">
+            Number of years:&nbsp;
+            <input className="cool-font" type="text" size="3" value={numYears} onChange={(e) => setNumYears(e.target.value)} />
+          </label>
+          <br />
+          <br />
+          <label className="cool-font">
+            Compounding interval:&nbsp;
+            <select className="cool-font" value={interval} onChange={(e) => setInterval(e.target.value)}>
+              <option className="cool-font" value="yearly">yearly</option>
+              <option className="cool-font" value="monthly">monthly</option>
+            </select>
+          </label>
+          <br />
+          <br />
+          <br />
+        <input type="submit" value="Calculate" className="cool-button" />
       </form>
       
       {finalAmount ? 
         <>
         <br />
         <br />
-        <h1>{`You will have $${numberWithCommas(finalAmount.toFixed(2))}`}</h1>
+          <h1 className="cool-big-font">{`You will have $${numberWithCommas(finalAmount.toFixed(2))}`}</h1>
         </>
         : 
         <></>
