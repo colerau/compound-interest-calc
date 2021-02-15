69b3378b2bc6266315a1136176414acc51105450
Cole Rau <colevrau@gmail.com>
2021-02-14 23:17:26 -0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import CompInterestForm from './components/CompInterestForm.js'
+import AmortizationForm from './components/AmortizationForm.js'
 
 function App() {
   return (
     <div className="App">
-      <CompInterestForm />
+
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Compound Interest Calculator</Link>
+            </li>
+            <li>
+              <Link to="/amortization-calculator">Amortization Calculator</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Switch>
+            <Route exact path="/">
+              <CompInterestForm />
+            </Route>
+            <Route path="/amortization-calculator">
+              <AmortizationForm />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/AmortizationForm.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { useState } from "react"
+
+const AmortizationForm = props => {
+  return (
+    <>
+      <h1 className="cool-font">
+        Amortization Calculator
+      </h1>
+    </>
+  )
+}
+
+export default AmortizationForm
