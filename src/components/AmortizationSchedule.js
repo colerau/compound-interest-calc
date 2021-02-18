4194ccbd527a2b531445d423aa0da05a374dd108
Cole Rau <colevrau@gmail.com>
2021-02-18 00:20:05 -0800

    on way to solve problem where last payment amount needs to account for rounding

--- src/components/AmortizationSchedule.js
+++ src/components/AmortizationSchedule.js
@@ -6,31 +6,53 @@ const AmortizationSchedule = props => {
   // need to get interest rate per month as a decimal
   let correctedInterestRate = interestRate / 12 * 0.01
 
+  const twoDecimals = num => {
+    return Number.parseFloat(num).toFixed(2)
+  }
+
   const createSched = (loanAmount, correctedInterestRate, numYears, monthlyPayment) => {
-  //   let balance = loanAmount
-  //   debugger
-  //   let roundedBalance = balance.toFixed(2)
+    let balance = loanAmount
+    let roundedBalance = twoDecimals(balance)
+    let finalBalance
 
-  //   let interest
-  //   let roundedInterest
+    let interest
+    let roundedInterest
 
-  //   let principal
-  //   let roundedPrincipal
+    let principal
+    let roundedPrincipal
+
+    let finalPaymentAmount
   
-  //   for (let i = 1; i <= numYears * 12; i++) {
-  //     interest = roundedBalance * correctedInterestRate
-  //     roundedInterest = interest.toFixed(2)
+    for (let i = 1; i <= numYears * 12; i++) {
+      interest = roundedBalance * correctedInterestRate
+      roundedInterest = twoDecimals(interest)
 
-  //     principal = monthlyPayment - roundedInterest
-  //     roundedPrincipal = principal.toFixed(2)
+      principal = monthlyPayment - roundedInterest
+      roundedPrincipal = twoDecimals(principal)
 
-  //     balance = balance - roundedPrincipal
-  //     roundedBalance = balance.toFixed(2)
+      balance = balance - roundedPrincipal
+      roundedBalance = twoDecimals(balance)
 
-  //     console.log(`The Interest for Payment ${i} is ${roundedInterest}`)
-  //     console.log(`The Principal for Payment ${i} is ${roundedPrincipal}`)
-  //     console.log(`The Balance for Payment ${i} is ${roundedBalance}`)
-  //   }
+      console.log(`The Interest for Payment ${i} is ${roundedInterest}`)
+      console.log(`The Principal for Payment ${i} is ${roundedPrincipal}`)
+      console.log(`The Balance for Payment ${i} is ${roundedBalance}`)
+    }
+
+    finalBalance = roundedBalance
+    finalPaymentAmount = finalBalance
+
+    interest = roundedBalance * correctedInterestRate
+    roundedInterest = twoDecimals(interest)
+
+    principal = monthlyPayment - roundedInterest
+    roundedPrincipal = twoDecimals(principal)
+
+    roundedBalance = roundedBalance - finalPaymentAmount
+
+    console.log(`The last payment amount is ${finalPaymentAmount}`)
+    console.log(`The last Interest is ${roundedInterest}`)
+    console.log(`The last Principal is ${roundedPrincipal}`)
+    console.log(`The final Balance is ${roundedBalance}`)
   }
 
   return (
